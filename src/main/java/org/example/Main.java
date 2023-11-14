f30114c9cf38fe7e3b92381f146fabe0f8e2cd96
OPT1MUSenpai <opt1musenpai@gmail.com>
2023-11-14 16:51:33 +0530

    test commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,6 +23,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to my Calculator");
         Main calc = new Main();
+        
 
         int choice = 5;
         do {
